85e1bbeaac46a53009401e13d6db47bbc11eb417
bougis <regis.bouchez@gmail.com>
2019-11-05 11:05:43 +0100

    implementation JMS

--- GestionVoiture-web/src/main/java/fr/epita/application/Application.java
+++ GestionVoiture-web/src/main/java/fr/epita/application/Application.java
@@ -2,15 +2,29 @@ package fr.epita.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.config.JmsListenerContainerFactory;
 
+import javax.jms.ConnectionFactory;
 
 
 @SpringBootApplication(scanBasePackages="fr.epita")
 //@EnableJpaRepositories("fr.epita")
+@EnableJms
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+    @Bean
+    public JmsListenerContainerFactory<?> myFactory(ConnectionFactory connectionFactory, DefaultJmsListenerContainerFactoryConfigurer configurer) {
+        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+        configurer.configure(factory,connectionFactory);
+        return factory;
     }
 }
--- GestionVoiture-web/src/main/java/fr/epita/controller/VoitureController.java
+++ GestionVoiture-web/src/main/java/fr/epita/controller/VoitureController.java
@@ -3,17 +3,23 @@ package fr.epita.controller;
 import fr.epita.entite.VoitureEntite;
 import fr.epita.service.VoitureService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class VoitureController {
     @Autowired
-    VoitureService voitureService;
+    private VoitureService voitureService;
+
+    @Autowired
+    private JmsTemplate jmsTemplate;
 
     @RequestMapping("/")
     public String index() {
         voitureService.creerVoiture(new VoitureEntite("Peugeot","308",20000));
+        jmsTemplate.convertAndSend("carNotification", "Hello");
+        System.out.println("notifaction envoyée");
         return "on affiche";
 
     }
